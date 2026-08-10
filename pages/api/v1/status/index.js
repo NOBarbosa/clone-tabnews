a2a0506c19d20fef6ce0f01772b1a17f05ca8a7c
Natalia Oliveira <nob.ufba@gmail.com>
2026-08-10 14:28:40 -0300

    feat: add comments

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,7 @@
 import database from "infra/database.js";
 
+//request -> o que vem de fora
+// response -> o que mandamos pra fora
 async function status(request, response) {
   const result = await database.query("SELECT 1 + 1 as sum;");
   console.log(result.rows);
