f0e45fa133e0d8914ba88caa6d7cbcf51462d4b1
Natalia Oliveira <nob.ufba@gmail.com>
2026-08-13 14:55:20 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,9 +3,39 @@ import database from "infra/database.js";
 //request -> o que vem de fora
 // response -> o que mandamos pra fora
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Acima da média" });
+  const databaseName = process.env.POSTGRES_DB;
+
+  const version = await database.query("SHOW server_version;");
+
+  const updatedAt = new Date().toISOString();
+  const maxConnections = await database.query("SHOW max_connections;");
+  // const activeConnections = await database.query(
+  //   "Select count(*) FROM pg_stat_activity where state = 'active';",
+  // );
+
+  // const dataBseConnectionResult = await database.query(
+  //   `Select count(*)::int from pg_stat_activity where datname ='${databaseName}'`,
+  // );
+
+  const dataBseConnectionResult = await database.query({
+    text: "Select count(*)::int from pg_stat_activity where datname =$1",
+    values: [databaseName],
+  });
+
+  const proprieties = {
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: version.rows[0].server_version,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        // active_connections: parseInt(activeConnections.rows[0].count),
+        active_connections: dataBseConnectionResult.rows[0].count,
+      },
+    },
+    // postgres_version: version.rows[0].version,
+    // active_connections: parseInt(activeConnections.rows[0].count),
+  };
+  response.status(200).json(proprieties);
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,32 @@
-test("GET to /api/v1/status should return 200", async () => {
+// test("GET to /api/v1/status should return status proprieties", async () => {
+//   const response = await fetch("http://localhost:3000/api/v1/status");
+
+//   const responseBody = await response.json();
+//   const convertedDate = new Date(responseBody.updated_at).toISOString();
+//   console.log(responseBody);
+
+//   expect(responseBody.updated_at).toEqual(convertedDate);
+//   // expect(responseBody.postgres_version).toBeDefined();
+//   // expect(responseBody.max_connections).toBeDefined();
+//   // expect(responseBody.active_connections).toBeDefined();
+// });
+test("GET to /api/v1/status should return status proprieties", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
+
+  const responseBody = await response.json();
+  const convertedDate = new Date(responseBody.updated_at).toISOString();
+  console.log(responseBody);
+
   expect(response.status).toBe(200);
+
+  expect(responseBody.updated_at).toEqual(convertedDate);
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.active_connections).toEqual(1);
+});
+
+test("Teste de SQL INJECTION", async () => {
+  const response = await fetch(
+    "http://localhost:3000/api/v1/status?databaseName=local_db",
+  );
 });
